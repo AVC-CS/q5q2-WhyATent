7c50a31670f92d7526ebf65b7823ef23a1b3e4d1
WhyATent <wwyatnt@gmail.com>
2026-04-13 13:26:40 -0700

    q5q2 Done. I created another variable, int result = 1, to represent the first value printed, printing it before the loop that prints the value when multiplied by a base number, printing all exponents up until the one provided by the user.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main()
 {
     int N, M;
+    int result = 1;
 
     cout << "Enter your base number: ";
     cin >> N;
     cout << "Enter your exponent: ";
     cin >> M;
-
-    cout << N << " to power number: ";
+    cout << N << " to power number: " << result << " ";
+    for(int i=0; i<M; i++){
+        result *= N;
+        cout << result << " ";
+    }
 
     // TODO
 
